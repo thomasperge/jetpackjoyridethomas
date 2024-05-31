0dcd669a38bb9fed291badce859a2cb0505bed81
thomasperge <thomaskauffmant74@gmail.com>
2024-05-31 21:55:16 +0200

    feat: adding restrat button

--- Assets/RW/Script/PauseGame.cs
+++ Assets/RW/Script/PauseGame.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
     public GameObject pauseCanvas; // Canvas qui s'affiche quand le jeu est en pause
     public Button pauseButton; // Bouton de pause
     public Button playButton; // Bouton de reprise
+    public Button restartButton; // Bouton de redémarrage
 
     private bool isPaused = false;
 
@@ -17,6 +19,7 @@ public class PauseGame : MonoBehaviour
         // Assurez-vous que le bouton pause est actif et le bouton play est désactivé
         pauseButton.gameObject.SetActive(true);
         playButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false); // Assurez-vous que le bouton restart est désactivé au début
     }
 
     public void Pause()
@@ -25,9 +28,10 @@ public class PauseGame : MonoBehaviour
         Time.timeScale = 0f; // Mettre le jeu en pause
         pauseCanvas.SetActive(true); // Afficher le Canvas de pause
 
-        // Désactiver le bouton pause et activer le bouton play
+        // Désactiver le bouton pause et activer les boutons play et restart
         pauseButton.gameObject.SetActive(false);
         playButton.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
     }
 
     public void Resume()
@@ -36,8 +40,19 @@ public class PauseGame : MonoBehaviour
         Time.timeScale = 1f; // Reprendre le jeu
         pauseCanvas.SetActive(false); // Masquer le Canvas de pause
 
-        // Activer le bouton pause et désactiver le bouton play
+        // Activer le bouton pause et désactiver les boutons play et restart
         pauseButton.gameObject.SetActive(true);
         playButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f; // Assurez-vous que le temps est de nouveau en marche
+        isPaused = false; // Réinitialiser l'état de pause
+        pauseCanvas.SetActive(false); // Masquer le Canvas de pause
+
+        // Recharger la scène actuelle
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
